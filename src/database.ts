f4c9168b2c904d126b3e68279458d3128ca2c1f1
vinithecsar <viniciuscesarbrito@gmail.com>
2023-07-21 17:01:55 -0300

    mais erros

--- src/database.ts
+++ src/database.ts
@@ -2,9 +2,9 @@ import { env } from "./env";
 import { knex as setupKnex, Knex } from "knex";
 
 export const config: Knex.Config = {
-  client: env.DATABASE_CLIENTE,
+  client: env.DATABASE_CLIENT,
   connection:
-    env.DATABASE_CLIENTE === "sqlite"
+    env.DATABASE_CLIENT === "sqlite"
       ? {
           filename: env.DATABASE_URL,
         }
