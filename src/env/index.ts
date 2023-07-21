4ff034faf0ce84a781fc9bf4c8c200f70152a64c
vinithecsar <viniciuscesarbrito@gmail.com>
2023-07-21 16:58:44 -0300

    erro no index.ts

--- src/env/index.ts
+++ src/env/index.ts
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "test") {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
-  DATABASE_CLIENTE: z.enum(["sqlite", "pg"]),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333), //coerce transforma em número
 });
